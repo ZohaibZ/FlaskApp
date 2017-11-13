ef9b17405b93857817b9f08ef0831d960223417f
ZohaibZ <zohiab.zahid.18@gmail.com>
2017-11-13 10:09:44 -0500

    some stuff

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
-# edit the URI below to add your RDS password and your AWS URL
-# The other elements are the same as used in the tutorial
-# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://my336Db:zohaibdatabase@my336instance.cffs7lx8bqq2.us-east-1.rds.amazonaws.com:3306/FlaskApp'
-
-# Uncomment the line below if you want to work with a local DB
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-
+SQLALCHEMY_DATABASE_URI = 'mysql://my336Db:zohaibdatabase@my336instance.cffs7lx8bqq2.us-east-1.rds.amazonaws.com:3306/FlaskApp'
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
