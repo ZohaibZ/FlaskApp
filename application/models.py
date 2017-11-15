91b7f91abe77a7f5ed0c79dfc986a4fb96bb2c29
ZohaibZ <zohiab.zahid.18@gmail.com>
2017-11-15 16:11:15 -0500

    update with mithu

--- application/models.py
+++ application/models.py
@@ -26,6 +26,9 @@ class bars(db.Model):
         self.state = state
         self.phone = phone
 
+    def __repr__(self):
+        return "<Bar '{}'>".format(self.name)
+
 class drinkers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
@@ -45,3 +48,32 @@ class drinkers(db.Model):
         self.city = city
         self.state = state
         self.review = review
+
+class brewery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    address = db.Column(db.String(128))
+    city = db.Column(db.String(30))
+    state = db.Column(db.String(30))
+    country = db.column(db.String(30))
+    phone = db.Column(db.String(30))
+    beer = db.relationship('beers', backref='brewery', lazy ='dynamic')
+
+    def __init__(self, id, name, address, city, state, country, phone):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.country = country
+        self.phone = phone
+
+class beers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brewery_id = db.Column(db.Integer, db.ForeignKey('brewery.id'))
+    name = db.Column(db.String(30))
+
+    def __init__(self, id, brewery_id, name):
+        self.id = id
+        self.brewery_id = brewery_id
+        self.name = name
